e893d09f0507e39e1a957d6aec61556b714d1d41
M V Harinandan <harinandanmv11@gmail.com>
2025-08-11 15:13:36 +0530

    Fetching and displaying data

--- app/src/main/java/com/example/dbconnection/Home_Page.java
+++ app/src/main/java/com/example/dbconnection/Home_Page.java
@@ -5,6 +5,12 @@ import android.os.Bundle;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.Cursor;
+
 public class Home_Page extends AppCompatActivity {
 
     @Override
@@ -12,5 +18,40 @@ public class Home_Page extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home);
+
+        TextView textView = findViewById(R.id.textView);
+        Button btn1 = findViewById(R.id.button);
+
+        SQLiteDatabase sql = openOrCreateDatabase("studentdb", MODE_PRIVATE, null);
+
+        sql.execSQL("CREATE TABLE IF NOT EXISTS student(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, pw TEXT)");
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                StringBuilder result = new StringBuilder();
+
+                Cursor cursor = sql.rawQuery("SELECT * FROM student", null);
+
+                if (cursor.getCount() == 0) {
+                    textView.setText("EMPTY RECORD");
+                    return;
+                }
+
+                while (cursor.moveToNext()) {
+                    int id = cursor.getInt(0);
+                    String name = cursor.getString(1);
+                    String pw = cursor.getString(2);
+
+                    result.append("ID: ").append(id).append("\n")
+                            .append("Name: ").append(name).append("\n")
+                            .append("Password: ").append(pw).append("\n");
+                }
+
+                cursor.close();
+
+                textView.setText(result.toString());
+            }
+        });
     }
 }

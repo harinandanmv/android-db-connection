b75f9916b12c2116d1e2aeca3a6138ad75a54b4e
M V Harinandan <harinandanmv11@gmail.com>
2025-08-11 15:44:17 +0530

    Deletion from database

--- app/src/main/java/com/example/dbconnection/Home_Page.java
+++ app/src/main/java/com/example/dbconnection/Home_Page.java
@@ -7,9 +7,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.Cursor;
+import android.widget.Toast;
 
 public class Home_Page extends AppCompatActivity {
 
@@ -21,6 +23,8 @@ public class Home_Page extends AppCompatActivity {
 
         TextView textView = findViewById(R.id.textView);
         Button btn1 = findViewById(R.id.button);
+        EditText editText = findViewById(R.id.editText3);
+        Button btn2 = findViewById(R.id.button2);
 
         SQLiteDatabase sql = openOrCreateDatabase("studentdb", MODE_PRIVATE, null);
 
@@ -53,5 +57,26 @@ public class Home_Page extends AppCompatActivity {
                 textView.setText(result.toString());
             }
         });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = editText.getText().toString();
+
+                Cursor cursor = sql.rawQuery("SELECT * FROM student WHERE name = '" + name + "'", null);
+
+                if (cursor.getCount() > 0) {
+                    cursor.close();
+                    String query = "DELETE FROM student WHERE name = '" + name + "'";
+                    sql.execSQL(query);
+                    Toast.makeText(Home_Page.this, "DELETED!!!", Toast.LENGTH_SHORT).show();
+                } else {
+                    cursor.close();
+                    Toast.makeText(Home_Page.this, "DOESN'T EXIST!!!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
     }
 }

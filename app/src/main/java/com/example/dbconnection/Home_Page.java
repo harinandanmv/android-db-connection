bf659285b01082b41944eda6835940a90419a38e
M V Harinandan <harinandanmv11@gmail.com>
2025-08-18 14:53:09 +0530

    Auto refresh and update bug fixes

--- app/src/main/java/com/example/dbconnection/Home_Page.java
+++ app/src/main/java/com/example/dbconnection/Home_Page.java
@@ -1,5 +1,6 @@
 package com.example.dbconnection;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -16,47 +17,36 @@ import android.widget.Toast;
 
 public class Home_Page extends AppCompatActivity {
 
+    private SQLiteDatabase sql;
+
+    TextView textView;
+    Button btn1;
+    EditText editText;
+    Button btn2;
+    Button btn3;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home);
 
-        TextView textView = findViewById(R.id.textView);
-        Button btn1 = findViewById(R.id.button);
-        EditText editText = findViewById(R.id.editText3);
-        Button btn2 = findViewById(R.id.button2);
-        Button btn3 = findViewById(R.id.button4);
+        textView = findViewById(R.id.textView);
+        btn1 = findViewById(R.id.button);
+        editText = findViewById(R.id.editText3);
+        btn2 = findViewById(R.id.button2);
+        btn3 = findViewById(R.id.button4);
 
-        SQLiteDatabase sql = openOrCreateDatabase("studentdb", MODE_PRIVATE, null);
+        sql = openOrCreateDatabase("studentdb", MODE_PRIVATE, null);
 
-        sql.execSQL("CREATE TABLE IF NOT EXISTS student(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, pw TEXT)");
+        sql.execSQL("CREATE TABLE IF NOT EXISTS student(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, password TEXT)");
+
+        refresh();
 
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                StringBuilder result = new StringBuilder();
-
-                Cursor cursor = sql.rawQuery("SELECT * FROM student", null);
-
-                if (cursor.getCount() == 0) {
-                    textView.setText("EMPTY RECORD");
-                    return;
-                }
-
-                while (cursor.moveToNext()) {
-                    int id = cursor.getInt(0);
-                    String name = cursor.getString(1);
-                    String pw = cursor.getString(2);
-
-                    result.append("ID: ").append(id).append("\n")
-                            .append("Name: ").append(name).append("\n")
-                            .append("Password: ").append(pw).append("\n");
-                }
-
-                cursor.close();
-
-                textView.setText(result.toString());
+                refresh();
             }
         });
 
@@ -72,6 +62,7 @@ public class Home_Page extends AppCompatActivity {
                     String query = "DELETE FROM student WHERE name = '" + name + "'";
                     sql.execSQL(query);
                     Toast.makeText(Home_Page.this, "DELETED!!!", Toast.LENGTH_SHORT).show();
+                    refresh();
                 } else {
                     cursor.close();
                     Toast.makeText(Home_Page.this, "DOESN'T EXIST!!!", Toast.LENGTH_SHORT).show();
@@ -87,5 +78,30 @@ public class Home_Page extends AppCompatActivity {
             }
         });
 
+    }
+    @SuppressLint("SetTextI18n")
+    public void refresh(){
+        StringBuilder result = new StringBuilder();
+
+        Cursor cursor = sql.rawQuery("SELECT * FROM student", null);
+
+        if (cursor.getCount() == 0) {
+            textView.setText("EMPTY RECORD");
+            return;
+        }
+
+        while (cursor.moveToNext()) {
+            int id = cursor.getInt(0);
+            String name = cursor.getString(1);
+            String password = cursor.getString(2);
+
+            result.append("ID: ").append(id).append("\n")
+                    .append("Name: ").append(name).append("\n")
+                    .append("Password: ").append(password).append("\n");
+        }
+
+        cursor.close();
+
+        textView.setText(result.toString());
     }
 }
